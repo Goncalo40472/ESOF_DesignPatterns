ed4f6cf292d5945068d2798ba88cfc945c0d42cd
Goncalo40472 <104788264+Goncalo40472@users.noreply.github.com>
2022-11-16 16:33:38 +0000

    Update Ex2.php

--- Ex2.php
+++ Ex2.php
@@ -46,7 +46,7 @@
 
         private static $instance;
 
-        private function __construct(){} //Função construtora
+        private function __construct(){//Função construtora} 
 
         public static function getDBInstance() : DatabaseExample {
 
